ede1f7abe5b043c59cb9b437b733b7ddb6c7379e
Brandon <bchaing@ucsd.edu>
2024-04-16 16:48:24 -0700

    Optimized rendering and storing of frames

--- text.py
+++ text.py
@@ -1,30 +1,24 @@
 from led_matrix import *
 from PIL import Image, ImageDraw, ImageFont
-from time import sleep 
 
-TEXT_COLOR = (255,255,255)
-BG_COLOR = (0,0,0)
+TEXT_COLOR = (0,255,255)
+BG_COLOR = (0,0,255)
 
 matrix = LEDMatrix(16,16,board.D18)
 
-font = ImageFont.load_default(12)
+font = ImageFont.load_default(size=12)
 text = "THE QUICK BROWN FOX JUMPED OVER THE LAZY DOG. the quick brown fox jumped over the lazy dog."
 text_width = int(font.getlength(text))
 
-canvas = Image.new("RGB", (text_width + 16, 16), BG_COLOR)
+# Create single canvas with text
+canvas = Image.new("RGB", (text_width + (16*2), 16), BG_COLOR)
 draw = ImageDraw.Draw(canvas)
 draw.text((16, 0), text, font=font, fill=TEXT_COLOR)
 
-viewport_x = 0
-
 # Pre-render frames
-frames = []
-while viewport_x <= canvas.width:
-    cropped_text = canvas.crop((viewport_x, 0, viewport_x + 16, 16))
-    frames.append(cropped_text)
-    viewport_x += 1
+frames = [canvas.crop((x, 0, x + 16, 16)) for x in range(canvas.width + 16)]
 
 # Display frames
-for f in frames:
-    matrix.image(f)
+for frame in frames:
+    matrix.image(frame)
     matrix.show()
